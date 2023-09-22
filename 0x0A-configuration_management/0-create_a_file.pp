a6b2dd1728f252f634bb6283119719e8b05c7187
Akram Freij <freij.akram@gmail.com>
2023-09-22 13:52:56 -0700

    sss

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # This puppet file creates a file called school in /tmp folder
 
-file {'/tmp/school':
-permission => '0744',
-owner      => 'www-data',
-group      => 'www-data',
-content    => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
