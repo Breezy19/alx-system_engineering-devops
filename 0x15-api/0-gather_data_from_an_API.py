3544ed1ec8c6440fbc15cb51ad93e765a55c8a09
Akram Freij <freij.akram@gmail.com>
2023-10-27 10:31:50 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,79 @@
 #!/usr/bin/python3
-"""
-This Python script retrieves information about a given employee's TODO list
-progress and their completed tasks from a given API endpoint. It takes an
-employee ID as a command-line argument and
-outputs the information in the specified format.
-"""
+""" module doc """
 import requests
 import sys
 
+
+def main():
+    """ def com """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    main()
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todo_url = "{}/todos?userId={}".format(base_url, employee_id)
+"""
 
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
-        employee_name = user_data.get("name")
+Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
+     *TITLE*
+     *TITLE*
+     *TITLE*
 
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
-        todo_data = todo_response.json()
-        total_tasks = len(todo_data)
-        done_tasks = sum(1 for task in todo_data if task.get("completed"))
 
-        print("Employee {} is done with tasks({}/{}):".format
-              (employee_name, done_tasks, total_tasks))
-        for task in todo_data:
-            if task.get("completed"):
-                print("\t{}".format(task.get("title")))
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        sys.exit(1)
+https://jsonplaceholder.typicode.com/users?id=1
+{
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+https://jsonplaceholder.typicode.com/todos?userId=5
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  ]
+
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
+  """
