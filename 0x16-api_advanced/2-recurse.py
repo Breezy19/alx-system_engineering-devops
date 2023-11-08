2e0be2285fb180ce3a9cc746742d60e4df732118
“Breezy19” <freij.akram@gmail.com>
2023-11-07 16:15:30 -0800

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-This module contains a function that queries the Reddit API
-and returns a list containing the titles of all hot articles
-for a given subreddit using recursion.
+A recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
+If no results are found or if it's an invalid subreddit, it returns None.
 """
+
 import requests
 
 
@@ -13,20 +14,21 @@ def recurse(subreddit, hot_list=None, after=None):
 
     :param subreddit: Name of the subreddit
     :param hot_list: Accumulator for hot article titles
-    :param after: Pagination parameter for next set of results
+    :param after: ID of the last article in the previous request
     :return: List of hot article titles or None if invalid subreddit
     """
-
     if hot_list is None:
         hot_list = []
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 (by /u/username)'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'akram'}
     params = {'limit': 100, 'after': after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-    if response.status_code != 200:
+    # Check if the subreddit is not valid or the request was redirected
+    if response.status_code != 200 or response.history:
         return None
 
     try:
@@ -35,20 +37,21 @@ def recurse(subreddit, hot_list=None, after=None):
         return None
 
     # Check if there are articles in the subreddit
-    children = data.get('data', {}).get('children', None)
-    if children is None:
+    posts = data.get('data', {}).get('children', None)
+    if not posts:
         return None
 
-    for child in children:
-        hot_list.append(child['data']['title'])
+    hot_list.extend(child['data']['title'] for child in posts)
 
     after = data['data'].get('after', None)
-    if after is None:
+    if after is None:  # No more pages, return the list
         return hot_list
 
+    # Recursively call with the next page's 'after' ID
     return recurse(subreddit, hot_list, after)
 
 
-if __name__ == "__main__":
-    # The code for interacting with the command line can be placed here
+if __name__ == '__main__':
+    # This section is typically used to parse command line arguments
+    # and is left as a placeholder for further implementation.
     pass
