b35f065fb13b2b4ad7150e230e09999ce2b59c20
“Breezy19” <freij.akram@gmail.com>
2023-11-07 16:06:40 -0800

    0x16-api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,54 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None.
+This module contains a function that queries the Reddit API
+and returns a list containing the titles of all hot articles
+for a given subreddit using recursion.
 """
-
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=None, after=None):
     """
-    Queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
+    Recursively queries the Reddit API for hot articles of a subreddit.
 
-    - If not a valid subreddit, return None.
+    :param subreddit: Name of the subreddit
+    :param hot_list: Accumulator for hot article titles
+    :param after: Pagination parameter for next set of results
+    :return: List of hot article titles or None if invalid subreddit
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"after": after},
-    )
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            hot_list.append(title)
-        after = req.json().get("data").get("after")
+    if hot_list is None:
+        hot_list = []
 
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
-    else:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 (by /u/username)'}
+    params = {'limit': 100, 'after': after}
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
         return None
+
+    try:
+        data = response.json()
+    except ValueError:
+        return None
+
+    # Check if there are articles in the subreddit
+    children = data.get('data', {}).get('children', None)
+    if children is None:
+        return None
+
+    for child in children:
+        hot_list.append(child['data']['title'])
+
+    after = data['data'].get('after', None)
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
+
+
+if __name__ == "__main__":
+    # The code for interacting with the command line can be placed here
+    pass
